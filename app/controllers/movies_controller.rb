b910ff5cfe149de731b01e24bb6faa0b93b1037f
Michael Cheng <mcheng0@chicagobooth.edu>
2016-11-16 15:27:37 -0600

    FINAL

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,47 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie =  Movie.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+  end
+
+  def create_row
+    @movie = Movie.new
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
+    @movie.save
+
+    redirect_to("/movies/#{@movie.id}")
+  end
+
+  def edit_form
+    @movie = Movie.find(params[:id])
+  end
+
+  def update_row
+    @movie=Movie.find(params[:id])
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
+    @movie.save
+    render("show")
+  end
+
+  def destroy
+      m =  Movie.find_by({ :id => params[:id] })
+      m.destroy
+      redirect_to("/movies")
+  end
+end
